8d878a885a121b07cf3f535b15ae24f53881738c
Jaime Eugenio <jaime.eugenio@gmail.com>
2016-05-06 00:00:19 -0300

    Starting the project structure

---
+++ dotnet/Lahar.Integration/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lahar.Integration")]
+[assembly: AssemblyDescription("Developer By JEUGENIO IT (contato@jeugenio.com.br)")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Lahar LTDA")]
+[assembly: AssemblyProduct("Lahar.Integration")]
+[assembly: AssemblyCopyright("Copyright ©  JE 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("09a8b4be-496b-49df-87e5-86c3441dd6b6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
